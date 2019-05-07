db2280e85fa15379a3cc9f34f67dfa0fd606dfbb
Iwan van Staveren <iwan.van.staveren@oracle.com>
2019-05-07 16:59:54 +0200

    Fix #64 use for single bind params

--- tests/ConnectionTest.php
+++ tests/ConnectionTest.php
@@ -5,14 +5,44 @@ use PHPUnit\Framework\TestCase;
 
 class ConnectionTest extends TestCase
 {
-    public function test_successful_connection()
+    protected $con = null;
+
+    /**
+     * Set up a new object
+     *
+     * @return null
+     */
+    public function setUp()
     {
-        $dsn      = 'oci:dbname=127.0.0.1:49161/xe';
-        $username = 'system';
-        $password = 'oracle';
+        $user = getenv("OCI_USER") ?: 'system';
+        $pwd = getenv("OCI_PWD") ?: 'oracle';
+        $dsn = getenv("OCI_DSN") ?: 'oci:dbname=127.0.0.1:49161/xe';
+        $this->con = new Oci8($dsn, $user, $pwd);
+    }
 
-        $connection = new Oci8($dsn, $username, $password);
+    /**
+     * Test if it is a valid object
+     *
+     * @return null
+     */
+    public function testObject()
+    {
+        $this->assertNotNull($this->con);
+    }
 
-        $this->assertInstanceOf(Oci8::class, $connection);
+    public function testBindParamSingle()
+    {
+        $stmt = $this->con->prepare('INSERT INTO person (name) VALUES (?)');
+        $var = 'Joop';
+        $this->assertTrue($stmt->bindParam(1, $var, PDO::PARAM_STR));
+    }
+
+    public function testBindParamMultiple()
+    {
+        $stmt = $this->con->prepare('INSERT INTO person, email (name) VALUES (:person, :email)');
+        $var = 'Joop';
+        $email = 'joop@world.com';
+        $this->assertTrue($stmt->bindParam(':person', $var, PDO::PARAM_STR));
+        $this->assertTrue($stmt->bindParam(':email', $email, PDO::PARAM_STR));
     }
 }
